e5397f02e23736070960886297ab51d7e1ebeba7
Gustavo <Gustavo@GUSTAVO-PC>
2022-07-03 22:10:53 -0300

    Adicionar arquivos de projeto.

---
+++ Prova Djalma Ex5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_Djalma_Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            while (n1 < 100)
+            {
+                n1 = n1 + 2;
+                Console.WriteLine(n1);
+            }
+            Console.ReadKey();
+            
+        
+            
+        }
+    }
+}
